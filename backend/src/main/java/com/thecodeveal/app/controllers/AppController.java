0bbfab3b51a3000055ffb1c32c00dedaf064ce50
Nayana Petkar <nayana.petkar@intelliswift.com>
2022-01-17 17:04:29 +0530

    database configure again

--- backend/src/main/java/com/thecodeveal/app/controllers/AppController.java
+++ backend/src/main/java/com/thecodeveal/app/controllers/AppController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+//@RequestMapping("/")
 public class AppController {
 	
 	
-	@GetMapping
+	@GetMapping("/")
 	public String testApp() {
 		return "Hello Spring Security!";
 	}
